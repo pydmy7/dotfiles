d28348f472349f32aa35c30e283a0788c672b8a1
user1 <pydmy7@gmail.com>
2026-03-04 20:49:27 +0800

    none

--- install-lazyvim.ps1
+++ install-lazyvim.ps1
@@ -2,4 +2,4 @@ Remove-Item -Recurse -Force -Path $env:LOCALAPPDATA\nvim
 Remove-Item -Recurse -Force -Path $env:LOCALAPPDATA\nvim-data
 
 # https://www.lazyvim.org
-# git clone https://github.com/pydmy7/nvim.git $env:LOCALAPPDATA\nvim
+git clone https://github.com/pydmy7/nvim.git $env:LOCALAPPDATA\nvim
