88c60795c22b6b438d92a9f330e3ccfd2613c3b0
wangyao <1822364545@qq.com>
2026-04-30 20:10:34 +0800

    update

--- scripts/launch-vsdev.ps1
+++ scripts/launch-vsdev.ps1
@@ -1,7 +1,7 @@
 $paths = @(
+    "C:\Program Files\Microsoft Visual Studio\18\Community\Common7\Tools\Launch-VsDevShell.ps1",
     "C:\Program Files\Microsoft Visual Studio\18\Enterprise\Common7\Tools\Launch-VsDevShell.ps1",
-    "C:\Program Files\Microsoft Visual Studio\18\Professional\Common7\Tools\Launch-VsDevShell.ps1",
-    "C:\Program Files\Microsoft Visual Studio\18\Community\Common7\Tools\Launch-VsDevShell.ps1"
+    "C:\Program Files\Microsoft Visual Studio\18\Professional\Common7\Tools\Launch-VsDevShell.ps1"
 )
 
 $vsDevShell = $paths | Where-Object { Test-Path $_ } | Select-Object -First 1
